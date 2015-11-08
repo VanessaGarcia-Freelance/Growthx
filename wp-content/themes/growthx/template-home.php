df40b055f53ac88058490514627df37e188091cb
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-07 22:51:26 -0800

    adding in Latest Founder Story to homepage

--- wp-content/themes/growthx/template-home.php
+++ wp-content/themes/growthx/template-home.php
@@ -26,12 +26,36 @@
             echo '<div class="col-sm-8">';
             //echo '<img src="#" />';
             echo '<h2><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></h2> ';
-            echo '<p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read Our Blog</a></p> ';
+            echo '<p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read Our Blog</a></p>';
             echo '</div>';
         }
     ?>
   </div>
 
+  <div class="latest-founder-story row">
+    <?php 
+      $args = array( 'post_type' => 'profile', 'numberposts' => '1' );
+      $recent_posts = wp_get_recent_posts( $args );
+      foreach( $recent_posts as $recent ){
+        $fieldArray = array( "user_id" => $recent["ID"] );
+        $headerText = types_render_field( "header-text", $fieldArray );
+        $headerImage = types_render_field( "header-image", array( "url" => "true", "proportional" => "true" ) );
+
+        ?>
+
+        <div class="story-entry container" style="background-image: url( <?php printf($headerImage);  ?> )">
+          <div class="row">
+          <div class="col-sm-6 col-sm-offset-6">
+          <h1><?php printf($headerText);  ?></h1>
+          <p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read the Founder Story</a></p>
+          </div>
+          </div>
+        </div>
+
+     <?php      
+      }
+    ?>
+  </div>
 
 
   <?php get_template_part('templates/content', 'page'); ?>
