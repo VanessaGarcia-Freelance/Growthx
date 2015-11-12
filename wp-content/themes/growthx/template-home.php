caff739f96c08dee89c2933baa2bee8a82028630
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-12 12:50:06 -0800

    styling and restructuring for the blog section of the homepage

--- wp-content/themes/growthx/template-home.php
+++ wp-content/themes/growthx/template-home.php
@@ -12,19 +12,39 @@
   <?php get_template_part('templates/page', 'header'); ?>
 
 
-  <div class="lastest-blog-post row">
-    <?php
-        $args = array( 'numberposts' => '1' );
-        $recent_posts = wp_get_recent_posts( $args );
-        foreach( $recent_posts as $recent ){
-            echo '<div class="col-sm-8">';
-            //echo '<img src="#" />';
-            echo '<h2><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></h2> ';
-            echo '<p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read Our Blog</a></p>';
-            echo '</div>';
-        }
-    ?>
+  <div class="lastest-blog-posts row">
+    <div class="featured-post row col-sm-offset-1 col-sm-10">
+      <?php
+          $args = array( 'numberposts' => '1', 'orderby' => 'post_date',
+    'order' => 'ASC' );
+          $recent_posts = wp_get_recent_posts( $args );
+          foreach( $recent_posts as $recent ){
+              echo '<div class="col-sm-7">';
+              //echo '<img src="#" />';
+              echo '<h2><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></h2> ';
+              echo '<p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read Our Blog</a></p>';
+              echo '</div>';
+          }
+      ?>
+      <?php //get_template_part('templates/svg', 'logo'); ?>
+    </div>
+
+    <div class="post-carousel row col-sm-offset-1 col-sm-10">
+      <?php
+          $args = array( 'numberposts' => '3' );
+          $recent_posts = wp_get_recent_posts( $args );
+          foreach( $recent_posts as $recent ){
+              echo '<div class="col-sm-4">';
+              //echo '<img src="#" />';
+              echo '<h2><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a></h2> ';
+              echo '<p class="readmore"><a href="' . get_permalink($recent["ID"]) . '">Read Our Blog</a></p>';
+              echo '</div>';
+          }
+      ?>
+    </div>
   </div>
+
+
 
   <div class="latest-founder-story row">
     <?php 
