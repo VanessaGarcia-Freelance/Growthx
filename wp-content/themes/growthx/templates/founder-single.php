8dd32c8c092918ca00f4f32545ff69ce27b6129f
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-12 20:21:38 -0800

    styling for the founders page

--- wp-content/themes/growthx/templates/founder-single.php
+++ wp-content/themes/growthx/templates/founder-single.php
@@ -3,54 +3,35 @@
   <?php 
     $postid = get_the_ID();
     $fieldArray = array( "user_id" => $postid );
-
     $headerText = types_render_field( "header-text", $fieldArray );
     $headerImage = types_render_field( "header-image", array( "url" => "true", "proportional" => "true" ) );
-
     $storyContent = types_render_field("story-content", array( "output" => "html" ));
-
     $quotes = types_child_posts("quote");
     $galleryImages = types_child_posts("gallery");
-
   ?>
 
-    <header style="background-image: url( <?php printf($headerImage);  ?> )">
-      <h1><?php printf($headerText);  ?></h1>
-      <!-- <h1 class="entry-title"><?php the_title(); ?></h1> -->
+    <header class="border" style="background-image: url( <?php printf($headerImage);  ?> )">
+      <div>
+        <div class="col-sm-6 col-sm-offset-1">
+          <!-- <h1 class="entry-title"><?php the_title(); ?></h1> -->
+          <h1><?php printf($headerText);  ?></h1>
+        </div>
+      </div>
     </header>
+
+
     <div class="entry-content">
       <?php printf($storyContent); ?>
 
-       <hr/><br/>
-
-      <div class="gallery">
-      <h4>Gallery</h4>
-        <?php 
-          foreach ($galleryImages as $slide) {
-            $image = $slide ->fields['gallery-image'];
-            $caption = $slide ->fields['image-caption'];
-
-              //echo $image;
-            echo "<div class='slide'>";
-            echo "<img src='" . $image . "' />";
-            echo "<caption>" . $caption . "</caption>";
-            echo "</div>";
-
-          }
-        ?>
-      </div>
-
-      <hr/><br/>
-
-
-      <h4>Quotes</h4>
+      <!-- <h4>Quotes</h4> -->
 
       <?php 
-        // using a for loop for now. Will need to break these into individual blocks then insert them throughout the content.
+        //using a for loop for now. Will need to break these into individual blocks then insert them throughout the content.
         foreach ($quotes as $quote) {
           $quoteStyle = $quote->fields['quote-style'];
       ?>
-        <blockquote class=="<?php $quote->fields['quote-style']; ?>">
+        <div class="row">
+        <blockquote class="<?php echo $quote->fields['quote-style'] ?>">
       <?php
         // will set this later to a class name
           if($quoteStyle == 4) {    
@@ -59,9 +40,28 @@
           echo $quote->fields['quote-text'];
       ?>
         </blockquote> 
+        </div>
       <?php  
         }
       ?>
+
+      <!-- <h4>Gallery</h4> -->
+      <div class="gallery-container">
+          <?php 
+            foreach ($galleryImages as $slide) {
+              $image = $slide ->fields['gallery-image'];
+              $caption = $slide ->fields['image-caption'];
+              echo "<div class='slide' style='background-image: url(" . $image . ")' >";
+              echo "<div class='border'>";
+              echo "<div class='caption-container'>";
+              echo "<div class='caption'><h4>" . $caption . "</h4></div>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+            }
+          ?>
+      </div>
+
 
     </div>
     <footer>
