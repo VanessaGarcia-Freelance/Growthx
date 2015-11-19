075138d519fb41d5774f21b27f3f2dbc9e410fd4
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-18 20:03:49 -0800

    matching styles for posts page as we have for founder stories.

--- wp-content/themes/growthx/templates/content-single.php
+++ wp-content/themes/growthx/templates/content-single.php
@@ -1,15 +1,71 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+  
+  <?php if (has_post_thumbnail( $post->ID ) ): ?>
+  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
+  
+    $quotes = types_child_posts("quote");
+    $galleryImages = types_child_posts("gallery");
+
+    ?>
+  <?php endif; ?>
+
+
+  <article class="story">
+    <header class="border" style="background-image: url('<?php echo $image[0]; ?>')">
+      <div>
+        <div class="col-sm-6 col-sm-offset-1">
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php //get_template_part('templates/entry-meta'); ?>
+        </div>
+      </div>
     </header>
+
+  
+
+
     <div class="entry-content">
       <?php the_content(); ?>
+
+      <?php 
+        //using a for loop for now. Will need to break these into individual blocks then insert them throughout the content.
+        foreach ($quotes as $quote) {
+          $quoteStyle = $quote->fields['quote-style'];
+      ?>
+        <div class="row">
+        <blockquote class="<?php echo $quote->fields['quote-style'] ?>">
+      <?php
+        // will set this later to a class name
+          if($quoteStyle == 4) {    
+            echo "<img src='" . $quote->fields['quote-image'] . "' />"; 
+          }
+          echo $quote->fields['quote-text'];
+      ?>
+        </blockquote> 
+        </div>
+      <?php  
+        }
+      ?>
+
+      <!-- <h4>Gallery</h4> -->
+      <div class="gallery-container">
+          <?php 
+            foreach ($galleryImages as $slide) {
+              $image = $slide ->fields['gallery-image'];
+              $caption = $slide ->fields['image-caption'];
+              echo "<div class='slide' style='background-image: url(" . $image . ")' >";
+              echo "<div class='border'>";
+              echo "<div class='caption-container'>";
+              echo "<div class='caption'><h4>" . $caption . "</h4></div>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+            }
+          ?>
+      </div>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php //comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
