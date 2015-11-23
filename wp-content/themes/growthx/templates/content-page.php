f9a296a37ca7166ff2360b41b0303f3de2054df4
Joseph White <joseph@clairaudience.com>
2015-11-23 13:34:00 -0800

    first round of QA style fixes

--- wp-content/themes/growthx/templates/content-page.php
+++ wp-content/themes/growthx/templates/content-page.php
@@ -1,8 +1,6 @@
-<?php the_content(); ?>
-
  <article class="page-story">
 
-     <div class="entry-content">
-		<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+     <div class="entry-content"><?php the_content(); ?>
 	</div>
 </article>
+		<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
