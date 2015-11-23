864e0317eef8923372a0498a67f1546c707eb89e
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-22 18:16:37 -0800

    adding related posts to bottom of single posts page

--- wp-content/themes/growthx/templates/content-single.php
+++ wp-content/themes/growthx/templates/content-single.php
@@ -64,14 +64,55 @@
       <?php endif; ?>
 
     </div>
-    <!-- <div class="share-bar container-fluid">
+    <div class="share-bar container-fluid">
       <div class="row">
-      <div class="share-btn col-sm-8 col-sm-offset-2">SHARE</div>
+      <?php echo do_shortcode( '[SSB_SHARE class_first="" class_second="" class_link="" class_icon="" layout="square" remove_inside="0" remove_counter="0"]' ); ?>
       </div>
-    </div> -->
-    <?php echo do_shortcode( '[SSB_SHARE class_first="" class_second="" class_link="" class_icon="" layout="square" remove_inside="0" remove_counter="0"]' ); ?>
-
+    </div>
     
+
+    <div class="related-posts container-fluid">
+      <div class="row">
+        <div class="posts-container col-sm-8 col-sm-offset-2">
+        <h2>OTHER POSTS YOU’LL LIKE</h2>
+
+        <div class="posts">
+          <?php $orig_post = $post;
+            global $post;
+            $categories = get_the_category($post->ID);
+            if ($categories) {
+            $category_ids = array();
+            foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+            $args=array(
+            'category__in' => $category_ids,
+            'post__not_in' => array($post->ID),
+            'posts_per_page'=> 5, // Number of related posts that will be displayed.
+            'caller_get_posts'=>1,
+            'orderby'=>'rand' // Randomize the posts
+            );
+            $my_query = new wp_query( $args );
+            if( $my_query->have_posts() ) {
+            // echo '<div id="perfect-related_by-category" class="clear"><h3>Related Posts</h3><ul>';
+            while( $my_query->have_posts() ) {
+            $my_query->the_post(); ?>
+            <li>
+             <a href="<?php the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>">
+             </a>
+             <div class="perfect-related_by-category">
+             <a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+             </div>
+            </li>
+            <? }
+            echo '</ul></div>';
+            } }
+            $post = $orig_post;
+            wp_reset_query(); ?>
+        </div>
+
+        </div>
+      </div>
+
+    </div>
 
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
